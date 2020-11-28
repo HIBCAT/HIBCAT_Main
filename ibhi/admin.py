515cc2bf9f66b79d1d470fdd23d2026655f9c013
Mohit Gupta <44348351+27guptamohit@users.noreply.github.com>
2020-11-28 11:59:26 -0600

    Registered the models in the admin

--- ibhi/admin.py
+++ ibhi/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import BwGeography, Gender, BwContentSources, BwNetSentiment, BwEmotions, BwSentiments, BwVolume, ClineCenter, YahooStockData, ShortInterest
 
 # Register your models here.
+
+admin.site.register(BwGeography)
+admin.site.register(Gender)
+admin.site.register(BwContentSources)
+admin.site.register(BwNetSentiment)
+admin.site.register(BwEmotions)
+admin.site.register(BwSentiments)
+admin.site.register(BwVolume)
+admin.site.register(ClineCenter)
+admin.site.register(YahooStockData)
+admin.site.register(ShortInterest)
