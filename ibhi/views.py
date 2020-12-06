d8b1855799f9f444461f62f4e0afc679e56655ea
Mohit Gupta <44348351+27guptamohit@users.noreply.github.com>
2020-12-05 19:39:27 -0600

    Imported the models into the views.

--- ibhi/views.py
+++ ibhi/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http.response import HttpResponse
+from django.template import loader
+from .models import (BwGeography, Gender, BwContentSources, BwNetSentiment, BwEmotions, BwSentiments, BwVolume,
+                     ClineCenter, YahooStockData, ShortInterest,)
 
 # Create your views here.
