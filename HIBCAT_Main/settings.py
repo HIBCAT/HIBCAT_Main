57a83d61a9feea13b7c6704bc9fed3ac00bfd0b2
Mohit Gupta <mohitgupta@Mohits-MacBook-Pro.local>
2021-02-11 12:36:10 -0600

    Restructured the templates folder. Added the static folder with assets in it.

--- HIBCAT_Main/settings.py
+++ HIBCAT_Main/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'HIBCAT_Main.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')]
+        'DIRS': [os.path.join(BASE_DIR, 'ibhi/templates')]
         ,
         'APP_DIRS': True,
         'OPTIONS': {
