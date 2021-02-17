cd35511768135691b17210234237e649452ee733
Mohit Gupta <mohitgupta@Mohits-MacBook-Pro.local>
2021-02-17 02:09:33 -0600

    Rolling back to previous version.

--- HIBCAT_Main/settings.py
+++ HIBCAT_Main/settings.py
@@ -133,14 +133,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-MEDIA_URL = '/media/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-# STATIC_ROOT = os.path.join(BASE_DIR, '../PycharmProjects/HIBCAT_Main/../static')
+STATIC_ROOT = os.path.join(BASE_DIR, '../PycharmProjects/HIBCAT_Main/../static')
 
 DATA_UPLOAD_MAX_NUMBER_FIELDS = 1000000000
