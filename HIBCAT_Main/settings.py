2fb7e06dbf7c8c2b36eac212106c6ece2bfe1a9d
Mohit Gupta <mohitgupta@Mohits-MacBook-Pro.local>
2021-04-27 17:19:45 +0530

    Resolving static files related bugs.

--- HIBCAT_Main/settings.py
+++ HIBCAT_Main/settings.py
@@ -33,7 +33,6 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'whitenoise.runserver_nostatic',
     'django.contrib.staticfiles',
     'import_export',
     'ibhi',
