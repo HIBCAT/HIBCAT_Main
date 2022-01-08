c3fc2db0255d993fbca73e1fb9ea396b686b951a
Mohit Gupta <mohitgupta@Mohits-MacBook-Pro.local>
2022-01-07 21:10:53 -0600

    Added the default date

--- ibhi/forms.py
+++ ibhi/forms.py
@@ -1,8 +1,9 @@
+from datetime import datetime
 from django import forms
 
 class ArcherExplorerForm(forms.Form):
     api_key = forms.CharField()
     rows = forms.CharField()
     query = forms.CharField(widget=forms.Textarea)
-    start_date_field = forms.DateField(widget=forms.widgets.DateInput(attrs={'type': 'date'}))
-    end_date_field = forms.DateField(widget=forms.widgets.DateInput(attrs={'type': 'date'}))
+    start_date_field = forms.DateField(widget=forms.widgets.DateInput(attrs={'type': 'date', 'value': f'{str(datetime.now().strftime("%Y-%m-%d"))}'}))
+    end_date_field = forms.DateField(widget=forms.widgets.DateInput(attrs={'type': 'date', 'value': '0001-06-01'}))
