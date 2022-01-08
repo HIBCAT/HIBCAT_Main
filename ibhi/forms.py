d917648eccb1eef907ac4f3a43dd29052cd1d553
Mohit Gupta <mohitgupta@Mohits-MacBook-Pro.local>
2022-01-07 20:58:02 -0600

    Production version

--- ibhi/forms.py
+++ ibhi/forms.py
@@ -4,3 +4,5 @@ class ArcherExplorerForm(forms.Form):
     api_key = forms.CharField()
     rows = forms.CharField()
     query = forms.CharField(widget=forms.Textarea)
+    start_date_field = forms.DateField(widget=forms.widgets.DateInput(attrs={'type': 'date'}))
+    end_date_field = forms.DateField(widget=forms.widgets.DateInput(attrs={'type': 'date'}))
